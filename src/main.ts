6e71b6b4b926f5f1621962d6db1338fdcf544ef7
Ricardo Maurizio Paul <memaurifu14@hotmail.com>
2024-10-22 17:07:15 +0200

    entities take shape

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,12 @@ import { Canvas } from "./canvas";
 
 function main() {
   const canvas = new Canvas(64 * 16, 64 * 9);
-  const rsquare = new Entity(100, 100, 100, 100, "red");
+  const square = new Entity(100, 100, 100, 100, "red");
   const floor = new Entity(0, canvas.height - 100, canvas.width, 100, "green");
-
-  // function loop() {
-  //   canvas.clear();
-  //   character.draw(canvas);
-  //   requestAnimationFrame(loop);
-  // }
+  var entities = [square, floor];
 
   canvas.clear();
-  floor.draw(canvas);
-  rsquare.draw(canvas);
+  entities.forEach((entity) => entity.draw(canvas));
 }
 
 main();
