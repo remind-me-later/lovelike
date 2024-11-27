938cb954e95c688e6df3d4144a07100aa4d36746
Ricardo Maurizio Paul <memaurifu14@hotmail.com>
2024-11-27 19:52:16 +0100

    bounding box intersects and contains

--- src/component/bounding_box.ts
+++ src/component/bounding_box.ts
@@ -1,4 +1,5 @@
 import { Component } from "./mod.ts";
+import { assertEquals } from "https://deno.land/std/assert/assert_equals.ts";
 
 export class BoundingBox extends Component {
 	public collidingLeft = false;
@@ -28,4 +29,44 @@ export class BoundingBox extends Component {
 	public get height() {
 		return this.#height;
 	}
+
+	public intersects(other: BoundingBox) {
+		return (
+			this.x < other.x + other.width &&
+			this.x + this.width > other.x &&
+			this.y < other.y + other.height &&
+			this.y + this.height > other.y
+		);
+	}
+
+	public contains(other: BoundingBox) {
+		return (
+			this.x < other.x &&
+			this.x + this.width > other.x + other.width &&
+			this.y < other.y &&
+			this.y + this.height > other.y + other.height
+		);
+	}
 }
+
+Deno.test("BoundingBox intersects", () => {
+	const box1 = new BoundingBox(0, 0, 10, 10);
+	const box2 = new BoundingBox(5, 5, 10, 10);
+	const box3 = new BoundingBox(15, 15, 10, 10);
+
+	assertEquals(box1.intersects(box2), true);
+	assertEquals(box1.intersects(box3), false);
+	assertEquals(box2.intersects(box3), false);
+});
+
+Deno.test("BoundingBox contains", () => {
+	const box1 = new BoundingBox(0, 0, 10, 10);
+	const box2 = new BoundingBox(5, 5, 10, 10);
+	const box3 = new BoundingBox(15, 15, 10, 10);
+	const box4 = new BoundingBox(1, 1, 5, 4);
+
+	assertEquals(box1.contains(box2), false);
+	assertEquals(box1.contains(box3), false);
+	assertEquals(box2.contains(box3), false);
+	assertEquals(box1.contains(box4), true);
+});
